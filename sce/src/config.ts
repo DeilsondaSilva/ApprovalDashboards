2e5335a3c3797b41f9415bc1797e040c29d0ee40
Deilson Da Silva <ddasilva@esri.com>
2021-06-07 11:24:06 -0700

    Update config.ts

--- sce/src/config.ts
+++ sce/src/config.ts
@@ -1,18 +1,18 @@
-// https://scetest-psportal.maps.arcgis.com/home/item.html?id=15484086ad20495e8a9b0035bd6ad8f6#settings
-export const appId = 'eecL6xGiImSr3UXE';
+// https://sce-psportal.maps.arcgis.com/home/item.html?id=018785c264cd4b0fa874f75fadc735c4#settings
+export const appId = 'KjKLJ1trY7Ii4Wvl';
 
-// https://scetest-psportal.maps.arcgis.com/home/group.html?id=75856f42e0634fc0806f5c1e5e140231#overview
-export const approverGroupId = '75856f42e0634fc0806f5c1e5e140231';
+// https://sce-psportal.maps.arcgis.com/home/group.html?id=160ba304f4df410cae24e9db4b40cb3f#overview
+export const approverGroupId = '160ba304f4df410cae24e9db4b40cb3f';
 
-// https://scetest.maps.arcgis.com/home/group.html?id=6096a67e52e345cba1483a8ed30ab5cb#overview
-export const genericGroupId = '6096a67e52e345cba1483a8ed30ab5cb';
+// https://sce-psportal.maps.arcgis.com/home/group.html?id=8eaab1aee100453a971c207918456bd2#overview
+export const genericGroupId = '8eaab1aee100453a971c207918456bd2';
 
-// https://scetest.maps.arcgis.com/home/item.html?id=46f691c47e1a4d72861fac4f64a5a28d
-export const groupsTableItemId = 'df1bc12812134c4eba3c40ea014c5565';
+// https://sce-psportal.maps.arcgis.com/home/item.html?id=03b2772f5ab3416984afca6c92f36620
+export const groupsTableItemId = '03b2772f5ab3416984afca6c92f36620';
 
-// https://scetest.maps.arcgis.com/home/item.html?id=d13ee7a039fb41749415c849d8087c58
+// https://sce-psportal.maps.arcgis.com/home/item.html?id=ca00ed0df7eb4bc7acf1a043bc9a4bd5#data
 export const requestAccessLayer = {
-    url: 'https://services3.arcgis.com/nk0ZWDI3lXr0vAXe/ArcGIS/rest/services/RequestAccess_View/FeatureServer/0',
+    url: 'https://services3.arcgis.com/xmPYozdyNDmkc79k/arcgis/rest/services/Prod_Public_Safety_Portal_Request_Access_View/FeatureServer/0',
     fieldNames: {
         objectid: 'objectid',
         globalid: 'globalid',
@@ -20,6 +20,7 @@ export const requestAccessLayer = {
         approvalStatus: 'approval_status',
         firstName: 'first_name',
         lastName: 'last_name',
+        username: 'username',
         organizationName: 'organization_name',
         creationDate: 'CreationDate',
         editDate: 'EditDate',
@@ -33,18 +34,18 @@ export const requestAccessLayer = {
     whereClause: 'approval_status IS NULL AND your_title IS NOT NULL',
 };
 
-export const parentPortalUrl = 'https://scetest.maps.arcgis.com/';
-export const childPortalUrl = 'https://scetest-psportal.maps.arcgis.com/';
+export const parentPortalUrl = 'https://sce.maps.arcgis.com/';
+export const childPortalUrl = 'https://sce-psportal.maps.arcgis.com/';
 export const webAppUrl = 'https://localhost:8080/';
-export const portalLoginUrl = 'https://overview.publicsafetyportaltest.sce.com/';
+export const portalLoginUrl = 'https://overview.publicsafetyportal.sce.com/';
 
 export const userDefaults = {
-    role: 'Mwpmp1BUH8gVCOj7',
+    role: 'Wa3n0U2AgluHhkcC',
     userLicenseType: 'creatorUT',
     userType: 'arcgisonly',
-    childGroups: ['7c7a8dab758141049cf02f87d79a2100'],
+    childGroups: ['4df8f05100c7487eaec64ce6ae3d7429'],
     parentGroups: [
-        '6096a67e52e345cba1483a8ed30ab5cb',
+        '8eaab1aee100453a971c207918456bd2',
         '1688d32c9bc6406ba8b14b0ec053b980',
         '95496f32309a44a5bbed142793cfacaa',
     ],
